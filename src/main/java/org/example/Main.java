5247dd52d0fd95fb36b07c44cf926b883680ca2b
bronypony099 <brony099pony@gmail.com>
2024-03-27 13:24:03 +0300

    FINAL24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Helloooooooooo and welcome!");
+        System.out.printf("Helloooooooo232oo and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 87; i <= 51006; i++) {
