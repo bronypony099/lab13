000af9116fdeb08c76f20a45688ea4b8554c6127
bronypony099 <brony099pony@gmail.com>
2024-03-27 13:29:38 +0300

    Commitmaster

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("HellooooooDoooo and welcome!");
+        System.out.printf("HellooooooDoooo and weldwcome!");
 
-        // Press Shift+F10 or clicgk the green arrow button in the gutter to run the code.
+        // Press Shift+F10 or clicgk th green arrow button in the gutter to run the code.
         for (int i = 87; i <= 51006; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
